706bd16306333b5ef469a31a14da32ca1743cc69
slimerence <39113678+slimerence@users.noreply.github.com>
2018-10-11 11:05:17 +1100

    up

--- app/routes/web.php
+++ app/routes/web.php
@@ -11,3 +11,7 @@ Route::get('/product','\Smartbro\Controllers\frontend\ProductsController@view');
 Route::get('/switch-language/{lang?}', '\Smartbro\Controllers\frontend\CustomPageController@switch_language')->name('switch_language');
 Route::get('/term', '\Smartbro\Controllers\frontend\CustomPageController@term');
 Route::get('/return', '\Smartbro\Controllers\frontend\CustomPageController@return');
+Route::get('/sitemap',function(){
+    return response()->view(_get_frontend_theme_path('pages.sitemap'))
+        ->header('Content-Type', 'xml');
+});
---
+++ nurse/pages/sitemap.blade.php
@@ -0,0 +1,77 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset
+        xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+    <!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
+
+
+    <url>
+        <loc>https://nurse357.com/</loc>
+        <lastmod>2018-10-10T23:00:35+00:00</lastmod>
+        <priority>1.00</priority>
+    </url>
+    <url>
+        <loc>https://nurse357.com/about</loc>
+        <lastmod>2018-10-10T23:00:35+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
+    <url>
+        <loc>https://nurse357.com/product</loc>
+        <lastmod>2018-10-10T23:00:35+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
+    <url>
+        <loc>https://nurse357.com/page/blog</loc>
+        <lastmod>2018-10-10T23:00:35+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
+    <url>
+        <loc>https://nurse357.com/contact-us</loc>
+        <lastmod>2018-10-10T23:00:35+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
+    <url>
+        <loc>https://nurse357.com/frontend/customers/login</loc>
+        <lastmod>2018-10-10T23:00:35+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
+    <url>
+        <loc>https://nurse357.com/frontend/customers/register</loc>
+        <lastmod>2018-10-10T23:00:35+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
+    <url>
+        <loc>https://nurse357.com/view_cart</loc>
+        <lastmod>2018-10-10T23:00:35+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
+    <url>
+        <loc>https://nurse357.com/404</loc>
+        <lastmod>2018-10-10T23:00:35+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
+    <url>
+        <loc>https://nurse357.com/term</loc>
+        <lastmod>2018-10-10T23:00:35+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
+    <url>
+        <loc>https://nurse357.com/return</loc>
+        <lastmod>2018-10-10T23:00:35+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
+    <url>
+        <loc>https://nurse357.com/frontend/customers/forget-password</loc>
+        <lastmod>2018-10-10T23:00:35+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
+    <url>
+        <loc>https://nurse357.com/frontend/place_order_checkout</loc>
+        <lastmod>2018-10-10T23:00:35+00:00</lastmod>
+        <priority>0.64</priority>
+    </url>
+
+
+</urlset>
